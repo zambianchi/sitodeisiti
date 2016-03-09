13bac610b5a10fc7401665763ee366aa1b2cfc85
Davide <davi98de@gmail.com>
2016-03-09 15:31:37 +0100

    initial commit

---
+++ SitoDeiSiti/SitoDeiSiti/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using SitoDeiSiti.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SitoDeiSiti.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string path = HttpContext.Server.MapPath("~/App_Data/data.xml");
+            XDocument xml = XDocument.Load(path);
+            List<Sito> siti = xml.Descendants("Sito").Select(d =>
+                new Sito
+                {
+                    Nome = d.Element("Nome").Value,
+                    Link = d.Element("Link").Value,
+                    PhotoLink = d.Element("Link").Value + "/Content/Images/foto.jpg"
+                }).ToList();
+
+            return View(siti);
+        }
+        public ActionResult Info()
+        {
+            string path = HttpContext.Server.MapPath("~/App_Data/data.xml");
+            XDocument xml = XDocument.Load(path);
+            
+            return View((object)xml.ToString());
+        }
+    }
+}
---
+++ SitoDeiSiti/SitoDeiSiti/Models/Sito.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SitoDeiSiti.Models
+{
+    public class Sito
+    {
+        public string Nome { get; set; }
+        public string Link { get; set; }
+        public string PhotoLink { get; set; }
+    }
+}
